df2f3173448b4a6ff8bbfac12eea6b5e66a8f15b
Naveen Gupta <90691303+codebynavi@users.noreply.github.com>
2024-04-17 13:17:32 +0530

    Upload Index, CSS, JS Files

---
+++ script.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    $('#btnEncode').click(function () {
+        var url = $('#result').val();
+        if (url == '') {
+            alert('Please Enter URL or Text in the input Box');
+        } else {
+            if (isEncoded(url)) {
+                alert("URL is already in encoded form!");
+                $(this).prop('disabled', true);
+            } else {
+                var encodedUrl = encodeURIComponent(url);
+                $('#result').val(encodedUrl);
+                $(this).prop('disabled', true);
+                $('#btnDecode').prop('disabled', false);
+            }
+        }
+
+    });
+
+    $('#btnDecode').click(function () {
+        var url = $('#result').val();
+        if (url == '') {
+            alert('Please Enter URL or Text in the input Box');
+        } else {
+            if (!isEncoded(url)) {
+                alert("URL is already in decoded form!");
+                $(this).prop('disabled', true);
+            } else {
+                var decodedUrl = decodeURIComponent(url);
+                $('#result').val(decodedUrl);
+                $(this).prop('disabled', true);
+                $('#btnEncode').prop('disabled', false);
+            }
+        }
+    });
+
+    $('#btnCopy').click(function () {
+        var resultText = $('#result').val();
+        copyToClipboard(resultText);
+    });
+
+    // Function to check if URL is already encoded
+    function isEncoded(url) {
+        try {
+            return decodeURIComponent(url) !== url;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    // Function to copy text to clipboard
+    function copyToClipboard(text) {
+        var $temp = $("<textarea>");
+        $("body").append($temp);
+        $temp.val(text).select();
+        document.execCommand("copy");
+        $temp.remove();
+        alert("Copied to clipboard!");
+    }
+
+    $('#btnRefresh').click(function () {
+        // Reload the current page
+        location.reload();
+    });
+
+    const currentYear = new Date().getFullYear();
+    $('#getYear').text(currentYear);
+});
